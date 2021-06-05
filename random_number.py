6e60ea70a04f5c3eb2ea019e452a8f24bba10232
kbsim <kbsim2004@gmail.com>
2021-06-05 08:49:25 +0800

    update code

--- random_number.py
+++
@@ -1,5 +0,0 @@
-# https://www.w3schools.com/python/module_random.asp
-
-import random
-
-print(random.seed(10))
---
+++ random_number/random_seed.py
@@ -0,0 +1,10 @@
+# https://www.w3schools.com/python/module_random.asp
+
+import random
+
+
+random.seed(10)
+print(random.random())
+
+random.seed(10)
+print(random.random())
